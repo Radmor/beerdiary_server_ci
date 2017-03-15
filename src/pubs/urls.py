b5d475e56290742ab2f48e6e5ffc94679f77ebc4
Piotr Hankiewicz <piotr.hankiewicz@student.put.poznan.pl>
2017-03-15 16:56:29 +0100

    cleanup

--- src/pubs/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include, url
-from .views import TestView
-urlpatterns = [
-    url(r'^create/$', TestView.as_view(), name='test'),
-]
--- src/pubs/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import TemplateView
-from django.views.generic import CreateView
-
-from .models import Pub
-
-class PubCreateView(CreateView):
-    model = Pub
